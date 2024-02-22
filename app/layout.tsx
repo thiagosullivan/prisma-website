c8286bf131bbe9c12ce5c20228afcb958b67996c
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-22 10:17:45 -0300

    chore: seting Rubik font and React Icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
+import Header from "./components/Header";
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={rubik.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,11 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      'prisma-blue': '#011935',
+      'prisma-gray': '#F4F5F7',
+      'prisma-orange': '#F26223',
+    },
   },
   plugins: [],
 };
