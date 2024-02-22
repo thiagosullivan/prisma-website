6993f2ea4182d8534645df848dd2f754f4d6bea9
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-22 11:58:36 -0300

    feat: add Brand component

---
+++ app/components/Brands.tsx
@@ -0,0 +1,8 @@
+const Brands = () => {
+    return (
+        <section className="w-full h-48 bg-[url('/Produtos.jpg')]">
+        </section>
+    );
+}
+ 
+export default Brands;
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
 import Image from "next/image";
 import PrismaLogo from '@/public/prisma-logo-2.jpg';
 import Hero from "./components/Hero";
+import Brands from "./components/Brands";
+import Services from "./components/Services";
+import Team from "./components/Team";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
+      <Brands />
+      <Services />
+      <Team />
     </main>
   );
 }
