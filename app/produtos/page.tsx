c51f9d2375cb497b95faf051fef9e7dd98d0f99e
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-23 17:20:36 -0300

    feat: add Product page

---
+++ app/produtos/page.tsx
@@ -0,0 +1,14 @@
+const Produtos = () => {
+    return (
+        <main className="max-w-screen-xl mx-auto px-4 py-12 flex flex-col justify-center mainnew">
+            <h1 className="text-prisma-blue text-center text-5xl font-semibold mb-8">
+                Os produtos da <span className="text-prisma-orange">Prisma</span>
+            </h1>
+            <p className="text-prisma-blue text-lg text-justify">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero sequi cumque pariatur mollitia odit consequatur hic deserunt nihil doloribus debitis totam necessitatibus dolor modi tenetur accusantium dolorem tempora ut nulla ex, eaque amet nobis in explicabo natus? Ut inventore qui dolore cupiditate? Ullam incidunt alias, sequi nemo dolor omnis voluptas odio eum doloribus, tempora consectetur consequuntur officiis illum ipsam voluptatibus eos iusto aliquam doloremque. Sint accusamus, ab nemo iusto beatae vel voluptate modi quod harum dolores eum expedita tempore unde impedit? Nisi eos est quia possimus libero non dolorem. Ipsam porro quos hic minus veritatis autem, ipsum atque nostrum neque?
+            </p>
+        </main>
+    );
+}
+ 
+export default Produtos;
