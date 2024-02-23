20dca9b1302cf5e668a554710a45230436f0d15a
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-02-23 18:00:40 -0300

    feat: add new domains on next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+            protocol: 'https',
+            hostname: 'images.unsplash.com'
+            },
+            {
+            protocol: 'https',
+            hostname: 'cdn.leroymerlin.com.br'
+            },
+        ]
+    }
+};
 
 export default nextConfig;
