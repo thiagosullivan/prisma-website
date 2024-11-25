68c0ae1837f681027700479831dc98072023cd28
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-25 08:52:26 -0300

    feat: changing description, added provider and toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,12 +3,15 @@ import { Rubik } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { Toaster } from "sonner";
+import Provider from "./components/Provider";
 
 const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Prisma - Acessórios para Construção",
-  description: "Prisma, excelência em acessórios plásticos para construção, destaca-se pelos Niveladores de Piso que unem qualidade, rapidez e confiança.",
+  description:
+    "Prisma, excelência em acessórios plásticos para construção, destaca-se pelos Niveladores de Piso que unem qualidade, rapidez e confiança.",
 };
 
 export default function RootLayout({
@@ -19,9 +22,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={rubik.className}>
-        <Header />
-        {children}
-        <Footer />
+        <Provider>
+          <Header />
+          {children}
+          <Footer />
+          <Toaster visibleToasts={1} position="top-right" richColors />
+        </Provider>
       </body>
     </html>
   );
