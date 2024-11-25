e4b5d41590520c980eaea2f4ce2d6b9a798ccf87
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-25 16:00:07 -0300

    feat: changing how to fetch products on page products and products component on home

--- app/(pages)/produtos/page.tsx
+++ app/(pages)/produtos/page.tsx
@@ -1,67 +1,63 @@
+import { db } from "@/lib/db";
 import Image from "next/image";
 import Link from "next/link";
 
-const data = [
-    {
-        'img': 'https://cdn.leroymerlin.com.br/products/revestimento_de_parede_galicia_cinza_34x50cm_artens_89881050_6201_600x600.jpg',
-        'title': 'Industrial construction',
-        'description': 'Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue.'
+const Produtos = async () => {
+  const products = await db.product.findMany({
+    orderBy: {
+      createdAt: "desc",
     },
-    {
-        'img': 'https://cdn.leroymerlin.com.br/products/revestimento_de_parede_galicia_cinza_34x50cm_artens_89881050_6201_600x600.jpg',
-        'title': 'Industrial construction Industrial construction',
-        'description': 'Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue.'
-    },
-    {
-        'img': 'https://cdn.leroymerlin.com.br/products/revestimento_de_parede_galicia_cinza_34x50cm_artens_89881050_6201_600x600.jpg',
-        'title': 'Industrial construction',
-        'description': 'Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue.'
-    },
-    {
-        'img': 'https://cdn.leroymerlin.com.br/products/revestimento_de_parede_galicia_cinza_34x50cm_artens_89881050_6201_600x600.jpg',
-        'title': 'Industrial',
-        'description': 'Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue. Waters make fish every without firmament saw had. Morning air subdue.'
-    },
-]
+  });
 
-const Produtos = () => {
-    return (
-        <main className="max-w-screen-xl mx-auto px-4 py-12 flex flex-col items-center mainnew mb-10">
-            <div className="mt-24">
-                <h1 className="text-prisma-blue text-center text-5xl font-semibold mb-8">
-                    Os produtos da <span className="text-prisma-orange">Prisma</span>
-                </h1>
-                <p className="text-prisma-blue text-lg text-center">
-                    A nossa lista de produtos de alta qualidade que oferecemos para você!
-                </p>
+  return (
+    <main className="max-w-screen-xl mx-auto px-4 py-12 flex flex-col items-center mainnew mb-10">
+      <div className="mt-24">
+        <h1 className="text-prisma-blue text-center text-5xl font-semibold mb-8">
+          Os produtos da <span className="text-prisma-orange">Prisma</span>
+        </h1>
+        <p className="text-prisma-blue text-lg text-center">
+          A nossa lista de produtos de alta qualidade que oferecemos para você!
+        </p>
 
-                <div className="grid grid-cols-2 gap-6 mt-14 max-sm:grid-cols-1">
-                    {data.map((product, index) => {
-                        return (
-                            <div key={index} className="border border-prisma-blue rounded-md overflow-hidden flex items-center p-4 max-lg:flex-col">
-                                <div className="h-[260px] w-full relative mr-5 max-lg:h-[200px] max-lg:mr-0 max-lg:mb-5 max-sm:w-[250px]">
-                                    <Image
-                                        src={product.img}
-                                        alt={product.title}
-                                        fill
-                                        className="h-[250px] max-lg:h-[200px] w-full rounded-md"
-                                        style={{
-                                            objectFit: 'cover',
-                                        }}
-                                    />
-                                </div>
-                                <div className="flex flex-col border-l border-prisma-blue pl-6 w-11/12 max-lg:w-full max-lg:border-l-0 max-lg:border-t max-lg:pt-5 max-lg:pl-0">
-                                    <h3 className="font-semibold text-lg mb-2">{product.title}</h3>
-                                    <p className="text-sm text-justify mb-8">{product.description}</p>
-                                    <Link href="https://wa.me/554384817211" rel="noopener noreferrer" target="_blank" className="bg-prisma-orange m-auto text-[#FFFFFF] px-6 py-3 hover:bg-prisma-orange-hover inline-block max-lg:mt-auto">Compre Agora</Link>
-                                </div>
-                            </div>
-                        )
-                    })}
+        <div className="grid grid-cols-2 gap-6 mt-14 max-sm:grid-cols-1">
+          {products.map((product) => {
+            return (
+              <div
+                key={product.id}
+                className="border border-prisma-blue rounded-md overflow-hidden flex items-center p-4 max-lg:flex-col"
+              >
+                <div className="h-[260px] w-full relative mr-5 max-lg:h-[200px] max-lg:mr-0 max-lg:mb-5 max-sm:w-[250px]">
+                  <Image
+                    src={product.imageUrl}
+                    alt={product.title}
+                    fill
+                    className="h-[250px] max-lg:h-[200px] w-full rounded-md"
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
                 </div>
-            </div>
-        </main>
-    );
-}
- 
+                <div className="flex flex-col border-l border-prisma-blue pl-6 w-11/12 max-lg:w-full max-lg:border-l-0 max-lg:border-t max-lg:pt-5 max-lg:pl-0">
+                  <h3 className="font-semibold text-lg mb-2">
+                    {product.title}
+                  </h3>
+                  <p className="text-sm text-justify mb-8">{product.content}</p>
+                  <Link
+                    href="https://wa.me/554384817211"
+                    rel="noopener noreferrer"
+                    target="_blank"
+                    className="bg-prisma-orange m-auto text-[#FFFFFF] px-6 py-3 hover:bg-prisma-orange-hover inline-block max-lg:mt-auto"
+                  >
+                    Compre Agora
+                  </Link>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </main>
+  );
+};
+
 export default Produtos;
--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -40,40 +40,48 @@ const Products = async () => {
   return (
     <section className="max-w-screen-xl mx-auto px-4 text-prisma-blue mt-10">
       <div>
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center mb-12">
           <h2 className="text-center text-sm text-prisma-orange max-lg:text-2xl">
             Nossos Produtos
           </h2>
           <p className="text-center text-5xl mt-5 max-lg:text-3xl mx-lg:mb-6">
             Oferecemos uma
-            <span className="text-prisma-orange font-semibold">variedade</span>
+            <span className="text-prisma-orange font-semibold">
+              {" "}
+              variedade{" "}
+            </span>
             de produtos <br /> de alta
-            <span className="text-prisma-orange font-semibold">qualidade</span>
+            <span className="text-prisma-orange font-semibold">
+              {" "}
+              qualidade{" "}
+            </span>
           </p>
         </div>
         <div className="grid grid-cols-2 gap-x-3 gap-y-8 justify-items-center max-lg:grid-cols-1">
-          {products.map((product) => {
+          {products.slice(0, 4).map((product) => {
             return (
               <div
                 key={product.id}
-                className="mt-24 flex items-center max-w-[700px] w-full gap-x-7 max-sm:flex-col max-sm:mt-6"
+                className="flex flex-col items-center max-w-[400px] w-full gap-x-7 max-sm:flex-col max-sm:mt-6"
               >
-                <div className="h-[220px] w-full relative max-sm:mb-6">
+                <div className="h-[300px] w-full relative max-sm:mb-6">
                   <Image
                     src={product.imageUrl}
                     alt="Produtos"
                     fill
-                    className="h-[260px] w-full rounded-md border border-gray-400"
+                    className="h-[250px] w-full rounded-md border border-gray-400"
                     style={{
                       objectFit: "cover",
                     }}
                   />
                 </div>
-                <div className="max-w-[330px] max-sm:max-w-none">
-                  <h4 className="font-semibold text-2xl text-prisma-blue">
+                <div className="">
+                  <h4 className="font-semibold mt-4 text-2xl text-prisma-blue">
                     {product.title}
                   </h4>
-                  <p className="mt-5 text-prisma-blue">{product.content}</p>
+                  <p className="mt-5 text-prisma-blue text-justify">
+                    {product.content}
+                  </p>
                   {/* <p className="mt-5 text-prisma-blue">
                     Waters make fish every without firmament saw had. Morning
                     air subdue. Waters make fish every without firmament saw
