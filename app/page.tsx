3a6cf0ae56f51916c06e50a43bcbafa96d582ebf
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-26 19:04:38 -0300

    feat: changes on home sections order

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,18 @@
-import Image from "next/image";
-import PrismaLogo from '@/public/prisma-logo-2.jpg';
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
-import Brands from "./components/Brands";
+import Phrase from "./components/Phrase";
+import Products from "./components/Products";
 import Services from "./components/Services";
 import Team from "./components/Team";
-import Products from "./components/Products";
-import Contact from "./components/Contact";
-import Phrase from "./components/Phrase";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
-      <Brands />
-      <Services />
-      {/* <Team /> */}
       <Phrase />
+      {/* <Brands /> */}
+      <Services />
+      <Team />
       <Products />
       <Contact />
     </main>
