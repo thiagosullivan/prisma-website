1540fa3ccf38bfa604f8390696ddc3dc88bad496
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-27 00:44:24 -0300

    fix: changing delete button on lists

--- app/(pages)/dashboard/servicesList.tsx
+++ app/(pages)/dashboard/servicesList.tsx
@@ -61,30 +61,29 @@ const ServicesList: React.FC<ServiceListProps> = ({
       </h3>
       <div className="grid grid-cols-2 gap-6 mt-14 max-sm:grid-cols-1">
         {services.map((product) => (
-          <div
-            key={product.id}
-            className="border border-prisma-blue rounded-md overflow-hidden flex items-center p-4 max-lg:flex-col relative"
-          >
+          <div key={product.id} className="relative">
             <button
-              className="bg-red-600 text-white rounded-2xl p-1 absolute top-2 right-2 z-10"
+              className="bg-red-600 text-white rounded-2xl p-1 absolute top-[-10px] right-2 z-10"
               onClick={() => deleteService(product.id)}
             >
               <X size={20} />
             </button>
-            <div className="h-[260px] w-full relative mr-5 max-lg:h-[200px] max-lg:mr-0 max-lg:mb-5 max-sm:w-[250px]">
-              <Image
-                src={product.imageUrl}
-                alt={product.title}
-                fill
-                className="h-[250px] max-lg:h-[200px] w-full rounded-md"
-                style={{
-                  objectFit: "cover",
-                }}
-              />
-            </div>
-            <div className="flex flex-col border-l border-prisma-blue pl-6 w-11/12 max-lg:w-full max-lg:border-l-0 max-lg:border-t max-lg:pt-5 max-lg:pl-0">
-              <h3 className="font-semibold text-lg mb-2">{product.title}</h3>
-              <p className="text-sm text-justify mb-8">{product.content}</p>
+            <div className="border border-prisma-blue rounded-md overflow-hidden flex items-center p-4 max-lg:flex-col">
+              <div className="h-[260px] w-full relative mr-5 max-lg:h-[200px] max-lg:mr-0 max-lg:mb-5 max-sm:w-[250px]">
+                <Image
+                  src={product.imageUrl}
+                  alt={product.title}
+                  fill
+                  className="h-[250px] max-lg:h-[200px] w-full rounded-md"
+                  style={{
+                    objectFit: "cover",
+                  }}
+                />
+              </div>
+              <div className="flex flex-col border-l border-prisma-blue pl-6 w-11/12 max-lg:w-full max-lg:border-l-0 max-lg:border-t max-lg:pt-5 max-lg:pl-0">
+                <h3 className="font-semibold text-lg mb-2">{product.title}</h3>
+                <p className="text-sm text-justify mb-8">{product.content}</p>
+              </div>
             </div>
           </div>
         ))}
