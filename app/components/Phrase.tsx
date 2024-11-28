ea5c0d5293300648bbb6bcd90e910d6016b3d437
Thiago Sullivan <thiagosullivan12@gmail.com>
2024-11-28 11:29:08 -0300

    feat: added new sentence on home section

--- app/components/Phrase.tsx
+++ app/components/Phrase.tsx
@@ -1,9 +1,27 @@
 const Phrase = () => {
-    return (
-        <section className="bg-prisma-blue">
-            <p className="max-w-screen-xl mx-auto text-[#FFFFFF] py-12 px-4 mt-12 text-3xl text-center"><span className="text-prisma-orange font-semibold text-4xl">&quot;</span> Transformando ambientes e elevando padrões estéticos, a <span className="text-prisma-orange font-semibold underline">Prisma</span> é a escolha definitiva em revestimentos, onde cada peça não apenas decora, mas conta uma história de sofisticação e qualidade incomparáveis. <span className="text-prisma-orange font-semibold text-4xl">&quot;</span></p>
-        </section>
-    );
-}
- 
+  return (
+    <section className="bg-prisma-blue">
+      <p className="max-w-screen-xl mx-auto text-[#FFFFFF] py-12 px-4 mt-12 text-3xl text-center">
+        <span className="text-prisma-orange font-semibold text-4xl">
+          &quot;
+        </span>
+        A{" "}
+        <span className="text-prisma-orange font-semibold uppercase">
+          Prisma
+        </span>{" "}
+        tem como missão transformar o mercado da construção com qualidade e
+        inovação. Nosso compromisso é oferecer soluções modernas e eficientes,
+        que elevem o padrão das obras, sempre alinhando tecnologia,
+        sustentabilidade e a satisfação dos nossos clientes. Transformando
+        ambientes e elevando padrões estéticos, a é a escolha definitiva em
+        revestimentos, onde cada peça não apenas decora, mas conta uma história
+        de sofisticação e qualidade incomparáveis.
+        <span className="text-prisma-orange font-semibold text-4xl">
+          &quot;
+        </span>
+      </p>
+    </section>
+  );
+};
+
 export default Phrase;
